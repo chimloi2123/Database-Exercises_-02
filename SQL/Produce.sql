cfbd59e40bf65fc70d38702db5dfdc2d5a2ebe11
lengckhoa <lngckhoagk14cqt@gmail.com>
2022-01-03 22:50:52 +0700

    Update Produce.sql

--- SQL/Produce.sql
+++ SQL/Produce.sql
@@ -149,16 +149,80 @@ AS
 GO
 ---Xem lich su luong thuong cua nhan vien
 CREATE PROCEDURE XEMLUONGTHUONG
-(
-	
-)
+
 AS 
 	BEGIN TRAN
 		SELECT * FROM LUONGTHUONG GROUP BY MANV
 	COMMIT
 GO
 
+--Xem so luong ton
+create proc sp_XemSLTonSP
+	@MACH int
+as
+begin tran
+	Select * from SANPHAM_CUAHANG where MACH = @MACH
+commit tran
 
+go
+
+--Xem CT nhap hang
+create proc sp_XemCTNhapHang
+	@MADNH int
+as
+begin tran
+	Select * from CT_DONNHAP where MADNH = @MADNH
+commit tran
+
+go
+
+--Xem CT giao hang
+create proc sp_XemCTGiaoHang
+	@MADGH int
+as
+begin tran
+	Select * from CT_GIAOHANG where MADGH = @MADGH
+commit tran
+
+go
+
+--Xem san pham ban chay
+create proc sp_XemSanPhamBanChay 
+as
+begin tran
+	Select MASP, SUM(SOLUONG) as TONGSOBAN
+	From CT_DONDATHANG
+	Group by MASP
+	Order by TONGSOBAN desc
+	--LIMIT 20
+commit
+
+go
+
+--Xem san pham ban cham
+create proc sp_XemSanPhamBanCham
+as
+begin tran
+	Select MASP, SUM(SOLUONG) as TONGSOBAN
+	From CT_DONDATHANG
+	Group by MASP
+	Order by TONGSOBAN
+	--LIMIT 20
+commit
+
+go
+
+--Xem so luong hang
+create proc sp_XemTongSoLuongHang
+as
+begin tran
+	Select sp.MASP, sp.TENSP, SUM(sc.SLTON)
+	From SANPHAM sp, SANPHAM_CUAHANG sc
+	Where sp.MASP = sc.MASP
+	Group by sp.MASP, sp.TENSP
+commit
+
+go
 
 
 
